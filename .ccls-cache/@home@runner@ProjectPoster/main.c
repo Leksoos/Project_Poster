f6f7b9bde5a403a2ea9c118df39e63615dc469b1
Leksoos <118820707+Leksoos@users.noreply.github.com>
2022-12-19 23:34:18 +0000

    Создана структура пользователся

---
+++ .ccls-cache/@home@runner@ProjectPoster/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct user {
+  char login;
+  char password;
+  int number_card;
+  int size_favorite;
+  bool admin;
+}
